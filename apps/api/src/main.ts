bb97e6af59a40ffb69f2a92bfca6825927669b23
Jonah Snider <jonah@jonahsnider.com>
2024-03-17 22:22:04 -0700

    refactor: replace boostrap function with TLA

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,15 +3,12 @@ import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 import { TrpcService } from './trpc/trpc.service';
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { abortOnError: process.env.NODE_ENV !== 'development' });
+const app = await NestFactory.create(AppModule, { abortOnError: process.env.NODE_ENV !== 'development' });
 
-	const trpcService = app.get(TrpcService);
+const trpcService = app.get(TrpcService);
 
-	trpcService.register(app);
+trpcService.register(app);
 
-	const configService = app.get(ConfigService);
+const configService = app.get(ConfigService);
 
-	await app.listen(configService.port);
-}
-bootstrap();
+await app.listen(configService.port);
