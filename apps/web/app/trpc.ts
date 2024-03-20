4a8a57e50dd97de4dbf7ae8162d4b9a5eb20c0a0
Jonah Snider <jonah@jonahsnider.com>
2024-03-20 14:15:18 -0700

    style: organize imports

--- apps/web/app/trpc.ts
+++ apps/web/app/trpc.ts
@@ -1,5 +1,5 @@
-import { transformer } from '@scores.frc.sh/api/src/trpc/transformer';
 import type { AppRouterType } from '@scores.frc.sh/api/src/trpc/app.router';
+import { transformer } from '@scores.frc.sh/api/src/trpc/transformer';
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
