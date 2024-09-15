3f5cf5adfd0ef977e3a51ae72aea9d402be530a2
Jonah Snider <jonah@jonahsnider.com>
2024-09-15 15:20:15 -0700

    feat(web): add webmanifest

---
+++ pwa-assets.config.ts
@@ -0,0 +1,31 @@
+import path from 'node:path';
+import { defineConfig } from '@vite-pwa/assets-generator/config';
+
+// NOTE: Bun is a bad package manager and isn't able to install the correct binary for sharp on my M1 Mac
+// If you want to run the icon generation script, you need to run npm install and then run the binary through npm
+
+const ROOT = path.join(__dirname, 'apps', 'web');
+
+// biome-ignore lint/style/noDefaultExport: This must be a default export
+export default defineConfig({
+	preset: {
+		transparent: {
+			sizes: [64, 144, 192, 256, 512],
+		},
+		maskable: {
+			sizes: [64, 144, 192, 256, 512],
+			padding: 0.3,
+			resizeOptions: {
+				background: '#121212',
+			},
+		},
+		assetName(type, size) {
+			return path.join(ROOT, 'public', `icons/${type}-${size.width}x${size.height}.png`);
+		},
+		apple: {
+			sizes: [],
+		},
+	},
+	images: [path.join(ROOT, 'app', 'icon.svg')],
+	root: ROOT,
+});
