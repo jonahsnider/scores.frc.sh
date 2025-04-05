549b631a4d471e8d30535b63c53c3b56215780dc
Jonah Snider <jonah@jonahsnider.com>
2025-04-04 20:00:59 -0700

    build: use drizzle-kit to run DB migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,10 +9,10 @@ const env = cleanEnv(process.env, {
 // biome-ignore lint/style/noDefaultExport: This must be a default export
 export default {
 	schema: 'apps/api/src/db/schema.ts',
-	driver: 'pg',
+	dialect: 'postgresql',
 	dbCredentials: {
-		connectionString: env.DATABASE_URL,
+		url: env.DATABASE_URL,
 	},
-	tablesFilter: ['!_prisma_migrations'],
 	out: 'drizzle',
+	tablesFilter: ['!_prisma_migrations'],
 } satisfies Config;
