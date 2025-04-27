8e914bf0c0a2a219f7dadeb50a4f81dd978fccee
Jonah Snider <jonah@jonahsnider.com>
2025-04-27 10:42:00 -0700

    build: fix dependency issues

--- apps/api/src/high-scores/dtos/event-year.dto.ts
+++ apps/api/src/high-scores/dtos/event-year.dto.ts
@@ -1,4 +1,4 @@
 import { z } from 'zod';
 import { YEAR_NEWEST, YEAR_OLDEST } from '../../cache-manager/cache-manager.constants';
 
-export const EventYear = z.number().int().min(YEAR_OLDEST).max(YEAR_NEWEST);
+export const EventYear = z.number({ coerce: true }).int().min(YEAR_OLDEST).max(YEAR_NEWEST);
--- apps/web/app/(scores)/[year]/layout.tsx
+++ apps/web/app/(scores)/[year]/layout.tsx
@@ -1,13 +1,10 @@
 import { EventYear } from '@scores.frc.sh/api/src/high-scores/dtos/event-year.dto';
 import { RedirectType, redirect } from 'next/navigation';
 import type { PropsWithChildren } from 'react';
-import { z } from 'zod';
-
-const ParamYear = z.coerce.number().pipe(EventYear);
 
 // biome-ignore lint/style/noDefaultExport: This has to be a default export
 export default function YearLayout({ params, children }: PropsWithChildren<{ params: { year: string } }>) {
-	const parsed = ParamYear.safeParse(params.year);
+	const parsed = EventYear.safeParse(params.year);
 
 	if (!parsed.success) {
 		redirect('/', RedirectType.push);
