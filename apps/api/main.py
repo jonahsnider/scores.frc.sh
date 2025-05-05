a34930b98a34b2adaf727a79ca7f393a6f7f5528
Jonah Snider <jonah@jonahsnider.com>
2025-05-05 15:50:48 -0700

    feat(api): define data schemas

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,8 +1,73 @@
-from fastapi import FastAPI
+from datetime import datetime, timedelta
+from enum import Enum
+from typing import Annotated
 
-app = FastAPI()
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
+
+app = FastAPI(title="scores.frc.sh API", version="2.0.0")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+year_path_param = Path(
+    title="The year to get the high scores for", example=2025, ge=2023
+)
+
+
+class MatchLevel(Enum):
+    QUALIFICATION = "Qualification"
+    PLAYOFF = "Playoff"
+
+
+class Event(BaseModel):
+    code: str
+    name: str
+    week_number: int
+
+
+class EventMatch(BaseModel):
+    number: int
+    level: MatchLevel
+
+
+class ScoreRecord(BaseModel):
+    event: Event
+    match: EventMatch
+    record_held_for: timedelta
+    score: int
+    timestamp: datetime
+    winning_teams: list[int]
+
+
+class HighScoresResponse(BaseModel):
+    high_scores: list[ScoreRecord]
+
+
+@app.get(
+    "/scores/year/{year}",
+    name="Get global high scores",
+    summary="Get global high scores",
+    description="Get the high scores for a specific year",
+    operation_id="getGlobalHighScores",
+    tags=["scores"],
+)
+async def global_high_scores(
+    year: Annotated[int, year_path_param],
+) -> HighScoresResponse:
+    return HighScoresResponse(high_scores=[])
+
+
+@app.get(
+    "/scores/year/{year}/event/{event}",
+    name="Get event high scores",
+    summary="Get event high scores",
+    description="Get the high scores for a specific event",
+    operation_id="getEventHighScores",
+    tags=["scores", "event"],
+)
+async def event_high_scores(
+    year: Annotated[int, year_path_param],
+    event: Annotated[
+        str, Path(title="The event code to get the high scores for", example="casj")
+    ],
+) -> HighScoresResponse:
+    return HighScoresResponse(high_scores=[])
