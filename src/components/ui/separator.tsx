94752ea8b23adf5c97e548d8e794113617ba28a7
Jonah Snider <jonah@jonahsnider.com>
2026-04-13 23:45:52 -0700

    feat: update all shadcn/ui components
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,19 +1,25 @@
-import { Separator as SeparatorPrimitive } from '@base-ui/react/separator';
+"use client"
 
-import { cn } from '@/lib/utils';
+import { Separator as SeparatorPrimitive } from "@base-ui/react/separator"
 
-function Separator({ className, orientation = 'horizontal', ...props }: SeparatorPrimitive.Props) {
-	return (
-		<SeparatorPrimitive
-			data-slot="separator"
-			orientation={orientation}
-			className={cn(
-				'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px data-[orientation=vertical]:self-stretch',
-				className,
-			)}
-			{...props}
-		/>
-	);
+import { cn } from "@/lib/utils"
+
+function Separator({
+  className,
+  orientation = "horizontal",
+  ...props
+}: SeparatorPrimitive.Props) {
+  return (
+    <SeparatorPrimitive
+      data-slot="separator"
+      orientation={orientation}
+      className={cn(
+        "shrink-0 bg-border data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
+        className
+      )}
+      {...props}
+    />
+  )
 }
 
-export { Separator };
+export { Separator }
